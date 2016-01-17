0bc47f0817c563bc13cd2604bbd2bfb5dfb2d435
lit-mazed <lit.mazed@gmail.com>
2016-01-17 22:13:35 +0600

    This is our master branch

---
+++ footer.php
@@ -0,0 +1,70 @@
+<footer>
+						<p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+						<a href="#contact" class="button button-big">Get in touch with me</a>
+					</footer>
+				</article>
+			</div>
+
+		<!-- Contact -->
+			<div class="wrapper wrapper-style4">
+				<article id="contact">
+					<header>
+						<h2>Want to hire me? Get in touch!</h2>
+						<span>Ornare nulla proin odio consequat  sapien vestibulum ipsum sed lorem.</span>
+					</header>
+					<div class="5grid">
+						<div class="row">
+							<div class="12u">
+								<form method="post" action="#">
+									<div class="5grid">
+										<div class="row">
+											<div class="6u">
+												<input type="text" name="name" id="name" placeholder="Name" />
+											</div>
+											<div class="6u">
+												<input type="text" name="email" id="email" placeholder="Email" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="text" name="subject" id="subject" placeholder="Subject" />
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<textarea name="message" id="message" placeholder="Message"></textarea>
+											</div>
+										</div>
+										<div class="row">
+											<div class="12u">
+												<input type="submit" class="button" value="Send Message" />
+												<input type="reset" class="button button-alt" value="Clear Form" />
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div class="row row-special">
+							<div class="12u">
+								<h3>Find me on ...</h3>
+								<ul class="social">
+									<li class="facebook"><a href="#">Facebook</a></li>
+									<li class="twitter"><a href="http://twitter.com/n33co">Twitter</a></li>
+									<li class="dribbble"><a href="http://dribbble.com/n33">Dribbble</a></li>
+									<li class="linkedin"><a href="#">LinkedIn</a></li>
+									<li class="tumblr"><a href="#">Tumblr</a></li>
+									<li class="googleplus"><a href="#">Google+</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<footer>
+						<p id="copyright">
+							by&copy; 2012 Untitled | Images: <a href="http://fotogrph.com">Fotogrph</a> + <a href="http://iconify.it">Iconify.it</a> | <a href="http://www.html5xcss3.com" target="_blank">Free Html5 Templates</a> : <a href="http://html5up.net/">HTML5 Up!</a>
+						</p>
+					</footer>
+				</article>
+			</div>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<!--  
+	Miniport 1.0 by HTML5 Up!
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Miniport by HTML5 Up!</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700" rel="stylesheet" />
+		<script src="js/jquery-1.8.3.min.js"></script>
+		<script src="css/5grid/init.js?use=mobile,desktop,1000px"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/5grid/core.css" />
+			<link rel="stylesheet" href="css/5grid/core-desktop.css" />
+			<link rel="stylesheet" href="css/5grid/core-1200px.css" />
+			<link rel="stylesheet" href="css/5grid/core-noscript.css" />
+			<link rel="stylesheet" href="style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
+		<!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
+	</head>
